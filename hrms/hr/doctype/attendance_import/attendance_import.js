1bc300bf431b7a0f82a2aa65fce29586d866bde0
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-10-11 22:52:45 +0700

    feat:add attendance import doctype

---
+++ hrms/hr/doctype/attendance_import/attendance_import.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Attendance Import", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
+frappe.ui.form.on('Attendance Import', {
+    refresh: function(frm) {
+        if (!frm.is_new()) {
+            frm.add_custom_button('Process File', () => {
+                frappe.call({
+                    method: 'hrms.hr.doctype.attendance_import.attendance_import.process_file', 
+                    args: { docname: frm.doc.name },
+                    callback: function(r) {
+                        if (!r.exc) {
+                            frappe.msgprint(__('File processed successfully!'));
+                        }
+                    }
+                });
+            });
+        }
+    }
+});
+
+
