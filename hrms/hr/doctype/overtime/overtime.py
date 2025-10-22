16122ed7067bc31158de9a413c602b58fe5ae506
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-10-22 16:53:26 +0700

    chore:adding the logic to calculate the total hours

--- hrms/hr/doctype/overtime/overtime.py
+++ hrms/hr/doctype/overtime/overtime.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from datetime import datetime
 
 
 class Overtime(Document):
-	pass
+	def before_save(self):
+		if self.start_time and self.end_time:
+			fmt = "%H:%M:%S"
+			start = datetime.strptime(str(self.start_time), fmt)
+			end = datetime.strptime(str(self.end_time), fmt)
+			diff = (end - start).seconds / 3600
+			self.total_hours = round(diff, 2)
