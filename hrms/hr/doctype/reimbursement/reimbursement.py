f82f5c6528a6a7fa5590010b9cc29436cfd92bda
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-10-22 11:47:34 +0700

    chore:update reimbursement function

--- hrms/hr/doctype/reimbursement/reimbursement.py
+++ hrms/hr/doctype/reimbursement/reimbursement.py
@@ -1,9 +1,27 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import nowdate, getdate
 
 
 class Reimbursement(Document):
-	pass
+	def on_submit(self):
+		if self.status == "Approved":
+			self.update_employee_reimbursement()
+			# self.create_additional_salary()
+	
+	def update_employee_reimbursement(self):
+		employee = frappe.get_doc("Employee", self.employee)
+
+		total_reimbursement = employee.total_reimbursement or 0
+		reimbursement_used = employee.reimbursement_used or 0
+
+		reimbursement_used += self.amount
+		balance = total_reimbursement - reimbursement_used
+
+		employee.reimbursement_used = reimbursement_used
+		employee.balance = balance
+		employee.save(ignore_permissions=True)
+
