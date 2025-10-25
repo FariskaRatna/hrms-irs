2b437e8e97f4c77b1711f198d411dd3c1c274a9f
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-10-25 22:48:58 +0700

    chore:edit new field on doctye and fixing the total hours calculation

--- hrms/hr/doctype/overtime/overtime.py
+++ hrms/hr/doctype/overtime/overtime.py
@@ -14,3 +14,33 @@ class Overtime(Document):
 			end = datetime.strptime(str(self.end_time), fmt)
 			diff = (end - start).seconds / 3600
 			self.total_hours = round(diff, 2)
+
+	def on_submit(self):
+		if self.status == "Approved":
+			self.create_overtime_slip()
+
+	def create_overtime_slip(self):
+		existing_slip = frappe.db.exists("Overtime Slip", {"reference_request": self.name})
+		if existing_slip:
+			frappe.msgprint("Overtime Slip already exists for this Overtime Request.")
+			return
+		
+		day_type = "Weekend" if self.is_weekend() else "Weekday"
+
+		slip = frappe.new_doc("Overtime Slip")
+		slip.employee = self.employee
+		slip.date = self.date
+		slip.total_hours = self.total_hours
+		slip.day_type = day_type
+		slip.reference_request = self.name
+		slip.status = "Pending"
+		slip.insert(ignore_permissions=True)
+		frappe.msgprint(f"Overtime Slip for {self.employee} successfully created.")
+	
+	def is_weekend(self):
+		if not self.date:
+			return False
+		# ubah dari string ke datetime
+		date_obj = datetime.strptime(str(self.date), "%Y-%m-%d").date()
+		# .weekday() -> 5 = Sabtu, 6 = Minggu
+		return date_obj.weekday() in (5, 6)
