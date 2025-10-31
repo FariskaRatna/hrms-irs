ae9b36548c4959b50817df1d8f76ace231860519
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-10-31 10:23:21 +0700

    feat:adding new doctype for loan settlement and calculation for loan application

--- hrms/hr/doctype/loan_application/loan_application.py
+++ hrms/hr/doctype/loan_application/loan_application.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import nowdate, getdate
 
 
 class LoanApplication(Document):
-	pass
+	def on_submit(self):
+		if self.status == "Approved"
+		self.update_employee_loan_application()
+		
+	def update_employee_loan_application(self):
+		employee = frappe.get_doc("Employee", self.employee)
+
+		total_loan = employee.total_loan or 0
+		total_installment = employee.installment or 0
+
+		total_installment += self.total_installment
+		balance = total_loan - total_installment
+
+		employee.installment = total_installment
+		employee.balance = balance
+		employee.save(ignore_permissions=True)
