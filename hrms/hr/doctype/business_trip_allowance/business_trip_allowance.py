11781fa45ac8c8b8c7f7fe6436914e2c9f8644f7
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-11-03 17:06:16 +0700

    chore:add field to counting weekend and weekday

--- hrms/hr/doctype/business_trip_allowance/business_trip_allowance.py
+++ hrms/hr/doctype/business_trip_allowance/business_trip_allowance.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate, add_days
 
 
 class BusinessTripAllowance(Document):
-	pass
+	def validate(self):
+		if self.business_trip:
+			self.calculate_allowance()
+
+	def calculate_allowance(self):
+		self.allowance
