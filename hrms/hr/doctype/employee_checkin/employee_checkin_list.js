447be133420ffe9280f74e7e156eee39592dfa28
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-11-11 21:47:53 +0700

    chore:new button for fetch data from employee checkin

--- hrms/hr/doctype/employee_checkin/employee_checkin_list.js
+++ hrms/hr/doctype/employee_checkin/employee_checkin_list.js
@@ -1,11 +1,14 @@
 frappe.listview_settings["Employee Checkin"] = {
 	add_fields: ["offshift"],
+
 	get_indicator: function (doc) {
 		if (doc.offshift) {
 			return [__("Off-Shift"), "yellow", "offshift,=,1"];
 		}
 	},
+
 	onload: function (listview) {
+		// Tombol Fetch Shifts
 		listview.page.add_action_item(__("Fetch Shifts"), () => {
 			const checkins = listview.get_checked_items().map((checkin) => checkin.name);
 			frappe.call({
@@ -16,5 +19,20 @@ frappe.listview_settings["Employee Checkin"] = {
 				},
 			});
 		});
+
+		// Tombol Sync Unlinked Attendances
+		listview.page.add_inner_button(__('Sync Unlinked Attendances'), function() {
+			frappe.call({
+				method: "hrms.hr.doctype.employee_checkin.employee_checkin.sync_unlinked_attendances",
+				freeze: true,
+				freeze_message: __("Checking and creating missing attendances..."),
+				callback: function(r) {
+					if (r.message) {
+						frappe.msgprint(r.message);
+						listview.refresh();
+					}
+				}
+			});
+		}, __("Attendance Actions"));
 	},
 };
