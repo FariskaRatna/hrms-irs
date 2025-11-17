1e13bb55bf97ecdc6fe3078bdebbc76e7af52f38
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-11-17 17:01:37 +0700

    chore:new field for hrd approval

--- hrms/hr/doctype/reimbursement/reimbursement.js
+++ hrms/hr/doctype/reimbursement/reimbursement.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Reimbursement", {
-// 	refresh(frm) {
+frappe.ui.form.on("Reimbursement", {
+	employee(frm) {
+        if (!frm.doc.employee) return;
 
-// 	},
-// });
+        frappe.db.get_value("Employee", frm.doc.employee, ["hrd_user"])
+            .then(r => {
+                frm.set_value("hrd_user", r.message.hrd_user);
+            });
+	},
+});
