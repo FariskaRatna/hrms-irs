0126dae3de9b5eaec49e0e28b005a96aac3db64d
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-12-09 18:26:58 +0700

    chore:delete like and comment icon on overtime calculation and overtime slip

---
+++ hrms/hr/doctype/overtime_slip/overtime_slip_list.js
@@ -0,0 +1,46 @@
+frappe.listview_settings["Overtime Slip"] = {
+    refresh: listview => {
+        $('span.level-item.list-liked-by-me.hidden-xs').remove();
+        $('span.list-row-like.hidden-xs').remove();
+        $('span.comment-count.d-flex.align-items-center').remove();
+        $('span.mx-2').remove();
+
+        setTimeout(() => {
+            $('.list-row .level-right').css({
+                "flex": "0 0 70px",
+                "max-width": "70px",
+                "padding": "0 4px",
+                "margin": "0",
+                "text-align": "right",
+                "white-space": "nowrap",
+                "overflow": "hidden"
+            });
+
+            $('.list-row-head .level-right').css({
+                "flex": "0 0 70px",
+                "max-width": "70px",
+                "padding": "0 4px",
+                "margin": "0",
+                "text-align": "right",
+                "white-space": "nowrap",
+                "overflow": "hidden"
+            });
+
+            $('.list-row-head .list-row-col').eq(5).css({
+                "flex": "0 0 180px",
+                "max-width": "200px",
+                "white-space": "nowrap",
+            });
+
+
+            $('.list-row-container .list-row').each(function () {
+                $(this).find('.list-row-col').eq(5).css({
+                    "flex": "0 0 180px",
+                    "max-width": "200px",
+                    "white-space": "nowrap",
+                });
+            });
+
+        }, 0)
+    }
+}
