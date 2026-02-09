55e99c1d421d1a0f50f2a03bc8375570246881df
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2026-02-09 14:45:57 +0700

    refactor:new doctype reimbursement report

--- hrms/hr/doctype/reimbursement_summary/reimbursement_summary.py
+++ hrms/hr/doctype/reimbursement_summary/reimbursement_summary.py
@@ -7,4 +7,37 @@ from frappe.utils import getdate, add_months
 
 
 class ReimbursementSummary(Document):
-	pass
+	def on_submit(self):
+		self.create_reimbursement_report()
+
+	def create_reimbursement_report(self):
+		if not self.employee:
+			return
+		
+		start_date = getdate(self.period_start)
+		end_date = getdate(self.period_end)
+
+		report = frappe.get_all(
+			"Reimbursement Report",
+			filters={
+				"from_date": start_date,
+				"to_date": end_date
+			},
+			limit=1
+		)
+
+		if report:
+			report = frappe.get_doc("Reimbursement Report", report[0].name)
+		
+		else:
+			report = frappe.new_doc("Reimbursement Report")
+			report.from_date = start_date
+			report.to_date = end_date
+			
+		report.append("reimbursement_report", {
+			"employee": self.employee,
+			"employee_name": self.employee_name,
+			"total_reimbursement": self.total_amount
+		})
+
+		report.save(ignore_permissions=True)
